c5c53d3f3f4aa116a68d3942755b992346e3f86f
shreyasingh1311 <shreyasinghshikhar1324@gmail.com>
2025-02-21 17:51:50 +0530

    project completed

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,28 @@
+export const THEMES = [
+  "light",
+  "dark",
+  "cupcake",
+  "bumblebee",
+  "emerald",
+  "corporate",
+  "synthwave",
+  "retro",
+  "cyberpunk",
+  "valentine",
+  "halloween",
+  "garden",
+  "forest",
+
+  "black",
+  "luxury",
+  "dracula",
+
+  "business",
+
+  "night",
+  "coffee",
+
+  "dim",
+
+  "sunset",
+  ];
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,39 @@
+import daisyui from "daisyui";
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+
+      "black",
+      "luxury",
+      "dracula",
+
+      "business",
+
+      "night",
+      "coffee",
+
+      "dim",
+
+      "sunset",
+    ],
+  },
+};
